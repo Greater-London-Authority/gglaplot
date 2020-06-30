8e5d9621b6ebabb148e68df23fa800da5b7669af
LibbyR <35563189+LiRogers@users.noreply.github.com>
2020-06-30 12:07:27 +0100

    Create geom2plotly.R
    ggplotly can now be used with ggla_line

---
+++ R/geom2plotly.R
@@ -0,0 +1,6 @@
+#' @name plotly_helpers
+#' @title Plotly helpers
+#' @description Helper functions to make it easier to automatically create plotly charts
+#' @export
+to_basic.GeomGLALine <- 
+  getFromNamespace("to_basic.GeomLine", asNamespace("plotly"))
